ffdbe1e4abe64310f5bbaac490b90c7c083bfbc3
chuzasjota <chuzas.j@gmail.com>
2019-11-15 17:15:50 -0600

    Add home url

--- wp-content/themes/Hotel-SJ/header.php
+++ wp-content/themes/Hotel-SJ/header.php
@@ -22,7 +22,7 @@
 	    <div class="container">
 	        <div class="row">
 	            <nav class="col-12 navbar navbar-expand-lg ">
-	                <a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo" class="img-fluid"></a>
+	                <a class="navbar-brand" href="<?php echo get_home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo" class="img-fluid"></a>
 	                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	                    <span class="bar-menu"></span>
 	                    <span class="bar-menu"></span>
