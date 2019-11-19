de65fd5150906465c34940576588c18e4fb0f123
chuzasjota <chuzas.j@gmail.com>
2019-11-19 15:22:58 -0600

    Create menu and add lightbox

--- wp-content/themes/Hotel-SJ/archive-menu.php
+++ wp-content/themes/Hotel-SJ/archive-menu.php
@@ -1,65 +1,79 @@
 <?php get_header(); ?>
 <div class="galeria pb-5">
-	<?php
-	    	// Argumentos para una busqueda de post type
-	$args = array(
-				'post_type' => 'banner_interna', // Nombre del post type
-				'order' => 'ASC',
-				'banners_interna' => 'Menu'
-			);
-	$banners = new WP_Query($args);
-	if ($banners->posts):
-	      // Foreach para recorrer el resultado de la busqueda
-		foreach ($banners->posts as $banner):
- 	 	 	$banner_name = $banner->post_title;
-			$banner = wp_get_attachment_url( get_post_thumbnail_id($banner->ID, 'full') );
-	?>
-	<section class="banner-interna container-fluid d-flex justify-content-center align-items-center font-arabic font-upper" style="background: url(<?php echo $banner; ?>) no-repeat center center / cover;">
-		<h1><?php echo $banner_name;?></h1>
-	</section>
-	<?php
-	endforeach;
-	endif; 
-	?>
-	<div class="container">
-		<div class="button-group">
-			<button class="is-checked">RESTAURANTE</button>
-		</div>
-		<section class="grid grid-galery">
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/1.jpg" alt="" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/2.jpg" alt="" class="img-fluid">	
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/3.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/4.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/5.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/6.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/7.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/8.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/9.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/10.jpg" class="img-fluid">
-			</div>
-			<div class="grid-item">
-				<img src="<?php echo get_template_directory_uri(); ?>/img/menu/11.jpg" class="img-fluid">
-			</div>
-		</section>
-	</div>
+  <?php
+        // Argumentos para una busqueda de post type
+  $args = array(
+        'post_type' => 'banner_interna', // Nombre del post type
+        'order' => 'ASC',
+        'banners_interna' => 'Menu'
+      );
+  $banners = new WP_Query($args);
+  if ($banners->posts):
+        // Foreach para recorrer el resultado de la busqueda
+    foreach ($banners->posts as $banner):
+      $banner_name = $banner->post_title;
+      $banner = wp_get_attachment_url( get_post_thumbnail_id($banner->ID, 'full') );
+  ?>
+  <section class="banner-interna container-fluid d-flex justify-content-center align-items-center font-arabic font-upper" style="background: url(<?php echo $banner; ?>) no-repeat center center / cover;">
+    <h1><?php echo $banner_name;?></h1>
+  </section>
+  <?php
+  endforeach;
+  endif; 
+  ?>
+  <div class="container">
+  <?php 
+    $args = array(
+      'post_type' => 'menu',
+      'order' => 'ASC'
+    );
+    $menus = new WP_Query($args);
+    if ($menus->posts):
+      $i = 0;
+  ?>
+      <div class="button-group">
+      <?php  
+        foreach ($menus->posts as $menu):
+      ?>
+          <button class="<?php echo $i == 0 ? 'is-checked' : '' ?>" data-filter=".<?php echo $menu->post_name; ?>" ><?php echo $menu->post_title; ?></button>
+      <?php endforeach; ?>
+      </div>
+  <?php endif; ?>
+    <section class="grid grid-galery">
+      <?php 
+        $args = array(
+          'post_type' => 'menu',
+          'order' => 'ASC'
+        );
+        $menus = new WP_Query($args);
+        if ($menus->posts):
+          foreach ($menus->posts as $menu):
+            $argsImages = array(
+              'post_parent'    => $menu->ID,
+              'post_type'      => 'attachment',
+              'numberposts'    => -1, // show all
+              'post_status'    => 'any',
+              'post_mime_type' => 'image',
+              'exclude'        =>  get_post_thumbnail_id($galeria->ID),
+              'orderby'        => 'menu_order',
+              'order'          => 'ASC'
+            );
+            $images = get_posts($argsImages);
+            if($images):
+              foreach($images as $image): 
+      ?>
+                <div class="grid-item <?php echo $menu->post_name; ?>" data-category="<?php echo $menu->post_name; ?>">
+                  <a data-fancybox="<?php echo $menu->post_name; ?>" href="<?php echo $image->guid; ?>">
+                    <img src="<?php echo $image->guid; ?>" class="img-fluid">
+                  </a>
+                </div>
+      <?php  
+              endforeach;
+            endif;
+          endforeach;
+        endif;
+      ?>
+    </section>
+  </div>
 </div>
 <?php get_footer(); ?>
