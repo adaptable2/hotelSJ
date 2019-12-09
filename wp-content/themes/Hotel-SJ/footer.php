cbc9d988f7089bd210846ee7c0ea2bb369ae2345
System Administrator <root@Camilo-wifi.local>
2019-12-09 18:24:29 -0500

    cambio de img

--- wp-content/themes/Hotel-SJ/footer.php
+++ wp-content/themes/Hotel-SJ/footer.php
@@ -1,5 +1,5 @@
 			<!-- Formulario -->
-			<section class="formulario container-fluid" id="contacto">
+			<section class="formulario container-fluid bg-white" id="contacto">
 				<div class="row">
 					<div class="offset-md-0 col-md-6 offset-lg-2 col-lg-4 pb-3">
 						<h2>
@@ -116,12 +116,12 @@
 			<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&display=swap" rel="preload" async/>
 			<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" async/>
 	 		<?php wp_footer(); ?>
-			<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js" defer></script>
-			<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js" defer></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js" defer></script>
-			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2tg95tRLV0b3omGIVAB3gD7WXVyjCNSU" defer></script>
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" defer></script>
-			<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js" defer></script>
+			<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
+			<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+			<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD2tg95tRLV0b3omGIVAB3gD7WXVyjCNSU"></script>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+			<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 			<script>
 	        conditionizr.config({
 	        	assets: '<?php echo get_template_directory_uri(); ?>',
--- wp-content/themes/Hotel-SJ/js/scripts.js
+++ wp-content/themes/Hotel-SJ/js/scripts.js
@@ -48,11 +48,11 @@
 		});
 		
 		//slick
-		// $('.carousel').slick({
-		// 	nextArrow: '<button type="button" class="slick-next">></button>',
-		// 	prevArrow: '<button type="button" class="slick-prev"><</button>',
-		// 	dots: true
-		// });
+		$('.carousel').slick({
+			nextArrow: '<button type="button" class="slick-next">></button>',
+			prevArrow: '<button type="button" class="slick-prev"><</button>',
+			dots: true
+		});
 		$('.carousel-banner').slick({
 			nextArrow: '<button type="button" class="slick-next">></button>',
 			prevArrow: '<button type="button" class="slick-prev"><</button>'
